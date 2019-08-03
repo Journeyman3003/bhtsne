Fix missing computation of sumQ for RKL gradient bh
-initialized data for fashion_mnist2500
-some debugging garbage that has to be cleaned up later

autoencoder.py
@@ -113,7 +113,7 @@ def show_encodings(inputs, latent_repr, outputs, filename="autoencoder-2dim-fash
     n = len(inputs)
     fig, axes = plt.subplots(2, n, figsize=(2 * n, 5))
     for i in range(n):
-        axes[0, i].set_title(str(i))
+        axes[0, i].set_title(FASHION_LABELDICT[i]) if "fashion" in filename else axes[0, i].set_title(str(i))
         axes[1, i].set_title('({0:.2f}, {1:.2f})'.format(float(latent_repr[i, 0]), float(latent_repr[i, 1])))
         axes[0, i].imshow(inputs[i].reshape(28, 28), cmap='gray')
         axes[1, i].imshow(outputs[i].reshape(28, 28), cmap='gray')
@@ -128,9 +128,9 @@ def show_encodings(inputs, latent_repr, outputs, filename="autoencoder-2dim-fash
 
 if __name__ == '__main__':
 
-    data_name = "mnist2500"
+    data_name = "fashion_mnist2500"
     #data_name = "mnist"
-    data, labels = mnist.load_mnist_data(False)
+    data, labels = mnist.load_fashion_mnist_data(False)
     # #data, labels = mnist.load_mnist_data(True)
     #
     data = mnist.mnist_1d_to_2d(data, num_observations=2500)
@@ -148,19 +148,20 @@ if __name__ == '__main__':
     # # #encoder.save("encoder.h5")
     # # #decoder.save("decoder.h5")
     # #
-    autoencoder = load_model(os.path.join("autoencoders", "mnist", "autoencoder.h5"))
-    encoder = load_model(os.path.join("autoencoders", "mnist","encoder.h5"))
-    decoder = load_model(os.path.join("autoencoders", "mnist","decoder.h5"))
+    autoencoder = load_model(os.path.join("autoencoders", "fashion_mnist", "autoencoder.h5"))
+    encoder = load_model(os.path.join("autoencoders", "fashion_mnist", "encoder.h5"))
+    decoder = load_model(os.path.join("autoencoders", "fashion_mnist", "decoder.h5"))
     # #
-    # data = np.reshape(data, (-1, 28, 28, 1)) / 255.0
-    data = np.reshape(data, (-1, 28, 28, 1))
+    data = np.reshape(data, (-1, 28, 28, 1)) / 255.0
+    # data = np.reshape(data, (-1, 28, 28, 1))
     # #
     inputs, latent_embedding, outputs = get_triple(data)
     # #
     indexes = list(map(lambda x: np.argmax(labels == x), np.arange(10)))
     # #
     # show_encodings(inputs[indexes], latent_embedding[indexes], outputs[indexes])
-    show_encodings(inputs[indexes], latent_embedding[indexes], outputs[indexes], filename="autoencoder-2dim-mnist2500.png")
+    show_encodings(inputs[indexes], latent_embedding[indexes], outputs[indexes],
+                   filename="autoencoder-2dim-fashion_mnist2500.png")
     # #
     filename = "initial_solution_" + data_name + "_autoencoder.pickle"
     # #

mnist.py
@@ -18,7 +18,24 @@ import urllib.request
 DEFAULT_DIR = 'MNIST'
 
 
-def load_mnist_data(all_data=False, dir_name=DEFAULT_DIR):
+def sample_data(data, labels, num_samples=2500):
+
+    # get unique labels
+    unique_labels = np.unique(labels)
+    num_samples_per_class = int(num_samples/len(unique_labels))
+    filter_index = []
+
+    for l in unique_labels:
+        indices = np.where(labels == l)
+        filter_index.extend(indices[0][:num_samples_per_class])
+
+    # filter for found indices
+    _sample_data = data[filter_index]
+    _sample_labels = labels[filter_index]
+
+    return _sample_data, _sample_labels
+
+def load_mnist_data(all_data=True, dir_name=DEFAULT_DIR):
     if all_data:
         pickle_mnist = os.path.join(dir_name, 'mnist.pkl.gz')
         if not os.path.exists(pickle_mnist):
@@ -46,8 +63,7 @@ def load_mnist_data(all_data=False, dir_name=DEFAULT_DIR):
         return mnist, label
 
 
-def load_fashion_mnist_data(dir_name=DEFAULT_DIR):
-
+def load_fashion_mnist_data(all_data=True, dir_name=DEFAULT_DIR):
     # init empty numpy arrays
 
     fashion_mnist_images = np.empty((0, 784))
@@ -82,7 +98,10 @@ def load_fashion_mnist_data(dir_name=DEFAULT_DIR):
         fashion_mnist_images = np.vstack((fashion_mnist_images, images))
         fashion_mnist_labels = np.hstack((fashion_mnist_labels, labels))
 
-    return fashion_mnist_images, fashion_mnist_labels
+    if all_data:
+        return fashion_mnist_images, fashion_mnist_labels
+    else:
+        return sample_data(fashion_mnist_images, fashion_mnist_labels, 2500)
 
 
 def mnist_1d_to_2d(data, num_observations=70000, img_rows=28, img_cols=28):
@@ -95,7 +114,8 @@ def mnist_2d_to_1d(data, num_observations=70000, img_rows=28, img_cols=28):
 
 if __name__ == '__main__':
     #x, label = load_mnist_data(True)
-    fashion_x, fashion_label = load_fashion_mnist_data()
+    fashion_x, fashion_label = load_fashion_mnist_data(all_data=False)
+
 
 
 